4595dd056225b3d857539b01f0d1c62c78f7aa02
rio <a20206394@pucp.edu.pe>
2024-04-29 18:54:27 -0500

    Probar funciones para crear la flota de camiones

--- 05 Punteros a funciones/Lab5 23-2/main.cpp
+++ 05 Punteros a funciones/Lab5 23-2/main.cpp
@@ -15,16 +15,16 @@ int main(int argc, char** argv) {
     double pesomaximo = 10;
     void *flota;
     
-    cargacamiones(flota, numcamiones, pesomaximo, leenumero, calculanumero,
-        "numeros.txt");
-    //qsort(flota, numcamiones, sizeof(void*), cmpnumero);
+    cargacamiones(flota, numcamiones, pesomaximo, leenumero, 
+        calculanumero, "numeros.txt");
+    qsort(flota, numcamiones, sizeof(void*), cmpnumero);
     muestracamiones(flota, numcamiones, imprimenumero, "reportenum.txt");
     
-//    numcamiones = 100; pesomaximo = 400;
-//    cargacamiones(flota, numcamiones, pesomaximo, leeregistro, calcularegistro,
-//        "pedidos3.csv");
-//    qsort(flota, numcamiones, sizeof(void*), cmpregistro);
-//    muestracamiones(flota, numcamiones, imprimeregistro, "reporteregistro.txt");
+    numcamiones = 100; pesomaximo = 400;
+    cargacamiones(flota, numcamiones, pesomaximo, leeregistro, calcularegistro,
+        "pedidos3.csv");
+    //qsort(flota, numcamiones, sizeof(void*), cmpregistro);
+    muestracamiones(flota, numcamiones, imprimeregistro, "reporteregistro.txt");
     
     return 0;
 }
