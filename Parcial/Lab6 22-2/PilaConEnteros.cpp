b55151301658b0adc49bab999704ae6010304446
rio <a20206394@pucp.edu.pe>
2024-05-06 19:55:15 -0500

    Agregar funcion para imprimir

--- Parcial/Lab6 22-2/PilaConEnteros.cpp
+++ Parcial/Lab6 22-2/PilaConEnteros.cpp
@@ -1,46 +1,6 @@
 
 #include "PilaConEnteros.h"
 
-void cargaarreglo(void *&arreglo, int (*cmp)(const void *, const void *), void *(*lee)(ifstream &), const char *nom){
-    ifstream arch(nom, ios::in);
-    
-    void *dato, **ptr = nullptr;
-    int cantDat = 0, cap = 0;
-    while(true){
-        dato = lee(arch);
-        if(dato == nullptr) break;
-        if(cantDat == cap) incrementarEspacios(ptr, cap, cantDat);
-        ptr[cantDat - 1] = dato; // Incrementar el contador antes de asignar el dato
-        cantDat++;
-    }
-    
-    arreglo = ptr;
-
-    qsort(arreglo, cantDat-1, sizeof(void *), cmp); // Cambiar cantDat-1 a cantDat
-    
-    // void **numeros = (void **)arreglo;
-    // for(int i = 0; numeros[i] ; i++){ // Cambiar el límite de iteración a cantDat
-    //     int *num = (int *)numeros[i];
-    //     cout << *num << endl;
-    // }
-}
-
-void incrementarEspacios(void **&ptr, int &cap, int &cantDat){
-    cap += INCREMENTO;
-
-    if(ptr == nullptr){
-        ptr = new void*[cap]{};
-        cantDat = 1;
-    }else{
-        void **aux = new void *[cap]{};
-        for(int i = 0; i < cantDat; i++){
-            aux[i] = ptr[i];
-        }
-        delete ptr;
-        ptr = aux;
-    }
-}
-
 void *leenumero(ifstream &arch){
     int num;
     arch>>num;
@@ -58,5 +18,13 @@ int cmpnumero(const void *a,const void *b){
     num2 = (int *)*aux2;
     
     return *num1 - *num2; 
+}
+
+void imprimenumero(ofstream &arch,void *dato){
+    // void **aux1 = (void **)dato;  
+    // int *num = (int *)*aux1;
+
+    int *num = (int *)dato;
+    arch<<*num<<endl;
 }
 
--- Parcial/Lab6 22-2/PilaConEnteros.h
+++ Parcial/Lab6 22-2/PilaConEnteros.h
@@ -1,3 +1,4 @@
+
 #pragma
 #include<iostream>
 #include<fstream>
@@ -5,8 +6,6 @@
 using namespace std;
 #define INCREMENTO 5
 
-void cargaarreglo(void *&arreglo,int (*cmp)(const void*,const void *),void* (lee)(ifstream &),const char *nom);
-void incrementarEspacios(void **&ptr, int &cap, int &cantDat);
 void* leenumero(ifstream &arch);
 int cmpnumero(const void *a,const void *b);
-
+void imprimenumero(ofstream &arch,void *dato);
