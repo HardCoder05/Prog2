e9c6740d5b59ff25e2856629fb39bfdd92187fd8
rio <a20206394@pucp.edu.pe>
2024-05-07 16:53:41 -0500

    Probar funciones nuevas

--- Parcial/Lab6 22-2/main.cpp
+++ Parcial/Lab6 22-2/main.cpp
@@ -5,22 +5,17 @@
 
 int main(){
    void *arreglo, *pilaini, *pilafin;
-
+    
     cargaarreglo(arreglo, cmpnumero, leenumero, "numeros.txt");
     cargapila(pilaini,arreglo);
-    // muevepila(pilaini,pilafin);
-    imprimepila(pilaini,imprimenumero,"repnumeros.txt");
+    muevepila(pilaini,pilafin);
+    imprimepila(pilafin,imprimenumero,"repnumeros.txt");
     
     cargaarreglo(arreglo, cmpregistro, leeregistro, "Medicinas.csv");
     cargapila(pilaini,arreglo);
-    //muevepila(pilaini,pilafin);
-    imprimepila(pilaini,imprimeregistro,"repmedicinas.txt");
+    muevepila(pilaini,pilafin);
+    imprimepila(pilafin,imprimeregistro,"repmedicinas.txt");
 
     return 0;
 }
-
-
-
-
-
 
