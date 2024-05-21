026be61b7f887d45f0c9283560d6299176b1e7b9
rio <rio88920@gmail.com>
2024-05-21 12:32:05 -0500

    Probar clase y sobrecarga

---
+++ 06 Clases/Lab6 23-2/main.cpp
@@ -0,0 +1,37 @@
+
+/* 
+ * File:   main.cpp
+ * Author: rio88
+ *
+ * Created on 21 de mayo de 2024, 09:20 AM
+ */
+
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+using namespace std;
+#include "Cliente.h"
+
+int main(int argc, char** argv) {
+    Cliente cliente[200]{};
+    
+//    ifstream arch("Clientes.csv", ios::in);
+//    int i = 0;
+//    while(true){
+//        if(arch>>cliente[i] == false) break;
+//        i++;
+//    }
+//    
+//    for(int j=0 ; j<i ; j++){
+//        int dni = cliente[j].GetDni();
+//        char *nombre = cliente[j].GetNombre();
+//        int telefono = cliente[j].GetTelefono();
+//        cout<<left<<setw(10)<<dni<<setw(40)<<nombre<<right<<setw(10)
+//            <<telefono<<endl;
+//    }
+    
+    
+    
+    return 0;
+}
+
