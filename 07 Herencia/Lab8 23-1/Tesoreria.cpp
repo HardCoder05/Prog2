b5c43240e115be5e4f4dd2257f86133ee81bb9b2
rio <rio88920@gmail.com>
2024-05-25 16:39:40 -0500

    Clase para manejar arreglos de otras clases

---
+++ 07 Herencia/Lab8 23-1/Tesoreria.cpp
@@ -0,0 +1,91 @@
+
+#include "Tesoreria.h"
+
+void Tesoreria::cargaEscalas(){
+    ifstream arch("escalas.csv", ios::in);
+
+    int codigo;
+    char c;
+    double precio;
+
+    while(true){
+        arch >> codigo;
+        if(arch.eof()) break;
+        arch >> c >> precio;
+
+        lescala[codigo - 1].setCodigo(codigo);
+        lescala[codigo - 1].setPrecio(precio);
+    }
+}
+
+void Tesoreria::cargaalumnos(){
+    ifstream arch("Alumnos.csv", ios::in);
+
+    int codigo, cantAlumnosPresencial = 0, cantAlumnosSemi = 0, cantAlumnosVir = 0, escala;
+    char c, tipo_de_alumno, nombre[60], licencia[60];
+    double recarga, descuento;
+
+    while(true){
+        arch >> tipo_de_alumno;
+        if(arch.eof()) break;
+        arch >> c >> codigo >> c;
+        arch.getline(nombre, 60, ',');
+        
+        if(tipo_de_alumno == 'P'){
+            arch >> escala >> c >> recarga;
+            lpresencial[cantAlumnosPresencial].llenarDatos(codigo, nombre, escala, recarga);
+            cantAlumnosPresencial++;
+        }
+        else if(tipo_de_alumno == 'S'){
+            arch >> escala >> c >> descuento;
+            lsemipresencial[cantAlumnosSemi].llenarDatos(codigo, nombre, escala, descuento);
+            cantAlumnosSemi++;
+        }
+        else{ // tipo_de_alumno == 'V'
+            arch >> escala >> c;
+            arch.getline(licencia, 30, '\n');
+            lvirtual[cantAlumnosVir].llenarDatos(codigo, nombre, escala, licencia);
+            cantAlumnosVir++;
+        }
+    }
+}
+
+
+
+void Tesoreria::imprime(){
+    ofstream arch("Reporte.txt", ios::out);
+
+    arch << "Presenciales" << endl;
+    arch << "Codigo" << setw(15) << "Nombre" << setw(42) << "Escala" << setw(10) << "Recarga" 
+        << setw(8) << "Total" << endl;
+    for(int i = 0; i < lpresencial[i].getCodigo() ; i++){
+        lpresencial[i].mostrarDatos(arch);        
+    }
+
+    arch << endl << "Semipresenciales" << endl;
+    arch << "Codigo" << setw(15) << "Nombre" << setw(42) << "Escala" << setw(10) << "Descuento" 
+        << setw(8) << "Total" << endl;
+    for(int i = 0; i < lsemipresencial[i].getCodigo() ; i++){
+        lsemipresencial[i].mostrarDatos(arch);        
+    }
+
+    arch << endl << "Virtuales: " << endl;
+    arch << "Codigo" << setw(15) << "Nombre" << setw(42) << "Escala" << setw(10) << "Licencia" 
+        << setw(8) << "Total" << endl;
+    for(int i = 0; lvirtual[i].getEscala() ; i++){
+        lvirtual[i].mostrarDatos(arch);       
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 07 Herencia/Lab8 23-1/Tesoreria.h
@@ -0,0 +1,41 @@
+
+#pragma once 
+#include<iostream>
+#include<fstream>
+#include<iomanip>   
+using namespace std;
+#include "Presencial.h"
+#include "Semipresencial.h"
+#include "Virtual.h"
+#include "Escala.h"
+
+class Tesoreria{
+    private:
+        Presencial lpresencial[50]{};
+        Semipresencial lsemipresencial[50]{};
+        Virtual lvirtual[50]{};
+        Escala lescala[10]{};
+    public:
+        void cargaEscalas();
+        void cargaalumnos();
+
+        void imprime();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
