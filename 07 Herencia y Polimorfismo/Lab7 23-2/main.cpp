a8ed7ab4e310005d8b3c67282a87d886175a6e60
rio <rio88920@gmail.com>
2024-05-28 13:15:32 -0500

    Probar

--- 07 Herencia y Polimorfismo/Lab7 23-2/main.cpp
+++ 07 Herencia y Polimorfismo/Lab7 23-2/main.cpp
@@ -6,15 +6,13 @@
  * Created on 28 de mayo de 2024, 09:13 AM
  */
 
-
+#include "Almacen.h"
 
 int main(int argc, char** argv) {
+    Almacen almacen;
     
-    
-    
-    
-    
-    
+    almacen.cargar_pedidos("pedidos.csv");
+    almacen.imprimir_ordenes_venta("ReportePrueba.txt");
     
     return 0;
 }
