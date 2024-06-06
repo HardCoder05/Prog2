7eabfa9f647229ae6806566f8a3f01911935f1b9
rio <rio88920@gmail.com>
2024-06-06 16:23:53 -0500

    Probar clase global

--- 08 Clases Autoreferenciadas/Lab10 23-1/main.cpp
+++ 08 Clases Autoreferenciadas/Lab10 23-1/main.cpp
@@ -6,16 +6,14 @@
  * Created on 6 de junio de 2024, 12:40 PM
  */
 
-
+#include "Tesoreria.h"
 
 int main(int argc, char** argv) {
-    
-    
-    
-    
-    
-    
-    
+    class Tesoreria caja;
+
+    caja.cargaAlumnos("Alumnos.csv");
+    caja.actualizaBoleta("escalas.csv");
+    caja.imprimeBoleta("reporteDePagos.txt");
+
     return 0;
 }
-
